d6997fa5c5b733d2ca05e0a4cfa093ee0787c5c8
Aneesh Das Mazumder <aneeshdasmazumder@gmail.com>
2024-05-10 22:47:25 +0530

    Single Responsibity Principle

---
+++ Single-Responsibilty-Principle/SolidPrinciple/EmployeeFinance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple
+{
+    /*
+     * This class is only responsible to handle Employee Finances.
+     */
+    public class EmployeeFinance
+    {
+        public double CalculatePay(Employee emp) { }
+    }
+}
